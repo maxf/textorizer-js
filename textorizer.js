b3f9fddf02108ddaae84519f008cdb6a0100cb41
Max Froumentin <max@lapin-bleu.net>
2010-03-10 17:33:59 +0100

    more control polishing, first actual textorize objects

---
+++ textorizer.js
@@ -0,0 +1,34 @@
+var Textorizer1 = new function() {
+  this.preview = function(params) {
+    this._params = params;
+    alert("implement me");
+  };
+  this.toPNG = function(params) {
+    this._params = params;
+    alert("implement me");
+  };
+  this.toSVG = function(params) {
+    this._params = params;
+    alert("implement me");
+  };
+
+  this._params = null;
+};
+
+var Textorizer2 = new function() {
+  this.preview = function(params) {
+    this._params = params;
+    alert("implement me");
+  };
+  this.toPNG = function(params) {
+    this._params = params;
+    alert("implement me");
+  };
+  this.toSVG = function(params) {
+    this._params = params;
+    alert("implement me");
+  };
+
+  this._params = null;
+};
+
