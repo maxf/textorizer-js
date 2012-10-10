7ed0462034315f04394ed8662532aacf0d4115d0
Max Froumentin <max@lapin-bleu.net>
2012-10-10 20:16:05 +0200

    all jslinted

--- t2_view.js
+++ t2_view.js
@@ -1,6 +1,6 @@
 /*jslint devel: true, browser: true, maxerr: 50, indent: 2 */
 
-var $, output_canvas, Textorizer;
+var $, output_canvas, Textorizer, FileReader, Fonts, changeOutputHeightTo;
 
 var defaults = {
     "text": "She knows, now, absolutely, hearing the white noise that is London, that Damien's theory of jet lag is correct: that her mortal soul is leagues behind her, being reeled in on some ghostly umbilical down the vanished wake of the plane that brought her here, hundreds of thousands of feet above the Atlantic. Souls can't move that quickly, and are left behind, and must be awaited, upon arrival, like lost luggage.", // http://www.williamgibsonbooks.com/books/pattern.asp#excerpt
@@ -53,111 +53,129 @@ function go(options) {
 
 // a thumbnail has been loaded
 function thumb_loaded(event) {
+  "use strict";
   // prepare the output canvas
-  var newImg = new Image();
+  var newImg = new Image(), aspectRatio;
   newImg.src = event.target.src;
   aspectRatio = newImg.width / newImg.height;
   output_canvas.height = defaults.output_height;
   output_canvas.width = defaults.output_height * aspectRatio;
-  $("#output_width_value").text(Math.floor(defaults.output_height*aspectRatio));
+  $("#output_width_value").text(Math.floor(defaults.output_height * aspectRatio));
   $("#output_height_value").text(Math.floor(defaults.output_height));
 
   // and render a preview
-  go({openWindow:false});
+  go({openWindow: false});
 }
 
 
-$(function() {
+$(function () {
+  "use strict";
+  inputCanvas = document.getElementById("input_canvas");
+  inputCanvasCtx = inputCanvas.getContext('2d');
+  $("#tabs").tabs();
 
-    inputCanvas = document.getElementById("input_canvas");
-    inputCanvasCtx = inputCanvas.getContext('2d');
-    $("#tabs").tabs();
-
-    $("#file_selector").change(function(e){
-                                  var fr = new FileReader();
-                                  fr.onload = function() {
-                                    $("#input_thumb").attr("src",fr.result);
-                                  };
-                                  fr.readAsDataURL(e.target.files[0]);
-                                });
+  $("#file_selector").change(function (e) {
+    var fr = new FileReader();
+    fr.onload = function () {
+      $("#input_thumb").attr("src", fr.result);
+    };
+    fr.readAsDataURL(e.target.files[0]);
+  });
 
     // only re activate the buttons when the image is loaded **FIXME - image could already be loaded (if we reselect the existing URL)
-    $("#input_thumb").load(function(e){
-                             thumb_loaded(e,0);
-                             $("#secondary_panel, #output_canvas, #input_thumb").show();
-                           });
+  $("#input_thumb").load(function (e) {
+    thumb_loaded(e, 0);
+    $("#secondary_panel, #output_canvas, #input_thumb").show();
+  });
 
 
     // no jquery on line below. We need the raw node values since we're operating on the attributes directly
-    output_canvas = document.getElementById("output_canvas");
+  output_canvas = document.getElementById("output_canvas");
 
-    $("#output_height").slider({min:100,
-                                max:2000,
-                                step: 1,
-                                value:defaults.output_height,
-                                slide: function(event, ui) {
-                                  changeOutputHeightTo(ui.value);
-                                }});
-
-    $("#opacity").slider({min:0,
-                         max:255,
-                         value:defaults.opacity,
-                         slide: function(event, ui) {
-                           $("#opacity_value").text(ui.value);
-                         }});
-
-    $("#opacity_value").text(defaults.opacity);
-    $("#output_height_value").text(defaults.output_height);
-    $("#preview_button").button();
-    $("#png_button").button();
-    $("#text").val(defaults.text);
-
-    $("#text_size").slider({min:4,
-                               max:50,
-                               step: .1,
-                               value:defaults.text_size,
-                               slide: function(event, ui) {
-                                 $("#text_size_value").text(ui.value);
-                               }});
-    $("#text_size_value").text(defaults.text_size);
-
-    $("#line_height").slider({min:0.5,
-                                 max:3,
-                                 step: .05,
-                                 value:defaults.line_height,
-                                 slide: function(event, ui) {
-                                   $("#line_height_value").text(ui.value);
-                                 }});
-    $("#line_height_value").text(defaults.line_height);
-
-    $("#saturation").slider({min:0,
-                                max:255,
-                                value:defaults.saturation,
-                                slide: function(event, ui) {
-                                  $("#saturation_value").text(ui.value);
-                                }});
-    $("#saturation_value").text(defaults.saturation);
-
-    $("#kerning").slider({min:-.5,
-                             max:.5,
-                             step: .05,
-                             value:defaults.kerning,
-                             slide: function(event, ui) {
-                               $("#kerning_value").text(ui.value);
-                             }});
-    $("#kerning_value").text(defaults.kerning);
-
-    $("#font_scale").slider({min:0,
-                                max:5,
-                                step: .05,
-                                value:defaults.font_scale,
-                                slide: function(event, ui) {
-                                  $("#font_scale_value").text(ui.value);
-                                }});
-    $("#font_scale_value").text(defaults.font_scale);
-
-    // populate the fonts dropowns
-    $("#font").html("<option>"+Fonts.join("</option><option>")+"</option>");
-
-
+  $("#output_height").slider({
+    min: 100,
+    max: 2000,
+    step: 1,
+    value: defaults.output_height,
+    slide: function (event, ui) {
+      changeOutputHeightTo(ui.value);
+    }
   });
+
+  $("#opacity").slider({
+    min: 0,
+    max: 255,
+    value: defaults.opacity,
+    slide: function (event, ui) {
+      $("#opacity_value").text(ui.value);
+    }
+  });
+
+  $("#opacity_value").text(defaults.opacity);
+  $("#output_height_value").text(defaults.output_height);
+  $("#preview_button").button();
+  $("#png_button").button();
+  $("#text").val(defaults.text);
+
+  $("#text_size").slider({
+    min: 4,
+    max: 50,
+    step: 0.1,
+    value: defaults.text_size,
+    slide: function (event, ui) {
+      $("#text_size_value").text(ui.value);
+    }
+  });
+  $("#text_size_value").text(defaults.text_size);
+
+  $("#line_height").slider({
+    min: 0.5,
+    max: 3,
+    step: 0.05,
+    value: defaults.line_height,
+    slide: function (event, ui) {
+      $("#line_height_value").text(ui.value);
+    }
+  });
+
+  $("#line_height_value").text(defaults.line_height);
+
+  $("#saturation").slider({
+    min: 0,
+    max: 255,
+    value: defaults.saturation,
+    slide: function (event, ui) {
+      $("#saturation_value").text(ui.value);
+    }
+  });
+
+  $("#saturation_value").text(defaults.saturation);
+
+  $("#kerning").slider({
+    min: -0.5,
+    max: 0.5,
+    step: 0.05,
+    value: defaults.kerning,
+    slide: function (event, ui) {
+      $("#kerning_value").text(ui.value);
+    }
+  });
+
+  $("#kerning_value").text(defaults.kerning);
+
+  $("#font_scale").slider({
+    min: 0,
+    max: 5,
+    step: 0.05,
+    value: defaults.font_scale,
+    slide: function (event, ui) {
+      $("#font_scale_value").text(ui.value);
+    }
+  });
+
+  $("#font_scale_value").text(defaults.font_scale);
+
+  // populate the fonts dropowns
+  $("#font").html("<option>" + Fonts.join("</option><option>") + "</option>");
+
+});
