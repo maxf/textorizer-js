a024dc5d502b568adef0e626762c84373f527443
Max <max@froumentin.net>
2014-01-27 12:25:27 +0000

    jshint

--- t2_view.js
+++ t2_view.js
@@ -1,6 +1,6 @@
 /*jslint devel: true, browser: true, maxerr: 50, indent: 2 */
 
-var $, output_canvas, Textorizer, FileReader, Fonts, changeOutputHeightTo;
+var $, output_canvas, Textorizer, Fonts;
 
 var defaults = {
     "text": "She knows, now, absolutely, hearing the white noise that is London, that Damien's theory of jet lag is correct: that her mortal soul is leagues behind her, being reeled in on some ghostly umbilical down the vanished wake of the plane that brought her here, hundreds of thousands of feet above the Atlantic. Souls can't move that quickly, and are left behind, and must be awaited, upon arrival, like lost luggage.", // http://www.williamgibsonbooks.com/books/pattern.asp#excerpt
@@ -18,7 +18,7 @@ var inputCanvas;
 var inputCanvasCtx;
 var aspectRatio;
 
-function go(options) {
+function go() {
   "use strict";
   $("#buttons").hide();
   $("#buttons_spinning_wheel").show();
@@ -71,10 +71,10 @@ $(function () {
   inputCanvas = document.getElementById("input_canvas");
   inputCanvasCtx = inputCanvas.getContext('2d');
 
-  $("#privacy").click(function (e) {
+  $("#privacy").click(function () {
     $("#privacy_popup").dialog();
   });
-  $("#cors").click(function (e) {
+  $("#cors").click(function () {
     $("#cors_popup").dialog();
   });
 
@@ -102,7 +102,7 @@ $(function () {
     min: 0,
     max: 255,
     value: defaults.opacity,
-    change: function (event, ui) {
+    change: function () {
       go();
     }
   });
@@ -114,7 +114,7 @@ $(function () {
     max: 50,
     step: 0.1,
     value: defaults.text_size,
-    change: function (event, ui) {
+    change: function () {
       go();
     }
   });
@@ -124,7 +124,7 @@ $(function () {
     max: 3,
     step: 0.05,
     value: defaults.line_height,
-    change: function (event, ui) {
+    change: function () {
       go();
     }
   });
@@ -134,7 +134,7 @@ $(function () {
     min: 0,
     max: 255,
     value: defaults.saturation,
-    change: function (event, ui) {
+    change: function () {
       go();
     }
   });
@@ -144,7 +144,7 @@ $(function () {
     max: 0.5,
     step: 0.05,
     value: defaults.kerning,
-    change: function (event, ui) {
+    change: function () {
       go();
     }
   });
@@ -154,7 +154,7 @@ $(function () {
     max: 5,
     step: 0.05,
     value: defaults.font_scale,
-    change: function (event, ui) {
+    change: function () {
       go();
     }
   });
