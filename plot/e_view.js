fe885971f88e6af6a80c18e5bbf64d50d29afda2
Max Froumentin <git@e97.me>
2024-06-30 10:15:51 +0100

    Add button to save SVG directly

--- plot/e_view.js
+++ plot/e_view.js
@@ -59,6 +59,20 @@ function go()
   };
 }
 
+function saveSvg(svgEl, name) {
+    svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    var svgData = svgEl.outerHTML;
+    var preface = '<?xml version="1.0" standalone="no"?>\r\n';
+    var svgBlob = new Blob([preface, svgData], {type:"image/svg+xml;charset=utf-8"});
+    var svgUrl = URL.createObjectURL(svgBlob);
+    var downloadLink = document.createElement("a");
+    downloadLink.href = svgUrl;
+    downloadLink.download = name;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
+
 // a thumbnail has been loaded
 function thumb_loaded(event) {
   "use strict";
@@ -121,12 +135,9 @@ id("margin").addEventListener("change", event => {
   go();
 });
 
-
-id("render_window").addEventListener("click", event => {
-  const newWin = window.open('','_blank');
-  newWin.title = 'Excoffizer SVG';
-  newWin.document.write(Excoffizer.excoffize(params));
-  newWin.focus();
-});
+id('save-button').addEventListener(
+  'click',
+  () => saveSvg(id('svg'), 'excoffizer.svg')
+);
 
 id("input_thumb").setAttribute("src", defaults.image_file);
